a0966091aab269abb192c1d3c4f40985a171456a
Tyler Neely <t@jujit.su>
2020-07-27 20:43:27 +0200

    Add some more variety to the incrementing test

--- hardware_timing/src/bin/incrementing.rs
+++ hardware_timing/src/bin/incrementing.rs
@@ -14,7 +14,9 @@ fn black_box<T>(item: &T) -> T {
 }
 
 fn main() {
-    println!("trying to increment a number 500 million times");
+    println!(
+        "trying to increment a number 500 million times"
+    );
 
     for _ in 0..100000 {
         Instant::now().elapsed();
@@ -26,10 +28,24 @@ fn main() {
     let timer = Instant::now();
     let mut count = 0;
     for i in 0..black_box(&N) {
+        count = i;
+    }
+    black_box(&count);
+    println!(
+        "single-threaded with write combined (normal +1): {:?}",
+        timer.elapsed()
+    );
+
+    let timer = Instant::now();
+    let mut count = 0;
+    for i in 0..black_box(&N) {
         unsafe { std::ptr::write_volatile(&mut count, i) };
     }
     black_box(&count);
-    println!("single-threaded with write_volatile: {:?}", timer.elapsed());
+    println!(
+        "single-threaded with write_volatile: {:?}",
+        timer.elapsed()
+    );
 
     let timer = Instant::now();
     let mut count = 0;
@@ -55,27 +71,69 @@ fn main() {
         timer.elapsed()
     );
 
-    let timer = Instant::now();
-    let count = AtomicUsize::new(0);
-    for i in 0..black_box(&N) {
-        count.compare_and_swap(i, i + 1, Ordering::SeqCst);
-    }
-    black_box(&count);
-    println!("single-threaded with SeqCst CAS: {:?}", timer.elapsed());
-
-    for threads in 1..3 {
-        for ordering in
-            [Ordering::Relaxed, Ordering::SeqCst, Ordering::AcqRel].iter()
+    for threads in &[1, 2, 4, 8] {
+        for ordering in [
+            Ordering::SeqCst,
+            Ordering::AcqRel,
+            Ordering::Release,
+            Ordering::Relaxed,
+        ]
+        .iter()
         {
-            let barrier1 = Arc::new(Barrier::new(threads + 1));
-            let barrier2 = Arc::new(Barrier::new(threads + 1));
+            let barrier1 =
+                Arc::new(Barrier::new(threads + 1));
+            let barrier2 =
+                Arc::new(Barrier::new(threads + 1));
             let count = Arc::new(AtomicUsize::new(0));
-            for _ in 0..threads {
+            for _ in 0..*threads {
                 let barrier1 = barrier1.clone();
                 let barrier2 = barrier2.clone();
                 let count = count.clone();
                 thread::spawn(move || {
-                    let mut current = count.load(Ordering::Acquire);
+                    let mut current =
+                        count.load(Ordering::Acquire);
+                    barrier1.wait();
+                    while current < black_box(&N) {
+                        current = count
+                            .fetch_add(1, *ordering)
+                            + 1;
+                    }
+                    barrier2.wait();
+                });
+            }
+            barrier1.wait();
+            let timer = Instant::now();
+            barrier2.wait();
+            println!(
+                "{} threads with {:?} atomic fetch_add: {:?}",
+                threads,
+                ordering,
+                timer.elapsed()
+            );
+        }
+    }
+
+    for threads in &[1, 2, 4, 8] {
+        for ordering in [
+            Ordering::SeqCst,
+            Ordering::AcqRel,
+            Ordering::Release,
+            Ordering::Relaxed,
+        ]
+        .iter()
+        {
+            let barrier1 =
+                Arc::new(Barrier::new(threads + 1));
+            let barrier2 =
+                Arc::new(Barrier::new(threads + 1));
+            let count = Arc::new(AtomicUsize::new(0));
+            for _ in 0..*threads {
+                let barrier1 = barrier1.clone();
+                let barrier2 = barrier2.clone();
+                let count = count.clone();
+                thread::spawn(move || {
+                    let mut current =
+                        count.load(Ordering::Acquire);
                     barrier1.wait();
                     while current < black_box(&N) {
                         let res = count.compare_and_swap(
@@ -104,11 +162,11 @@ fn main() {
         }
     }
 
-    for threads in 1..3 {
+    for threads in &[1, 2, 4, 8] {
         let barrier1 = Arc::new(Barrier::new(threads + 1));
         let barrier2 = Arc::new(Barrier::new(threads + 1));
         let count = Arc::new(Mutex::new(0));
-        for _ in 0..threads {
+        for _ in 0..*threads {
             let barrier1 = barrier1.clone();
             let barrier2 = barrier2.clone();
             let count = count.clone();
@@ -126,6 +184,10 @@ fn main() {
         barrier1.wait();
         let timer = Instant::now();
         barrier2.wait();
-        println!("{} threads with a Mutex: {:?}", threads, timer.elapsed());
+        println!(
+            "{} threads with a Mutex: {:?}",
+            threads,
+            timer.elapsed()
+        );
     }
 }
